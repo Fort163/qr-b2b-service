f7430c99c3e59021477eef325964c190e1eb6835
Павел <front-91@mail.ru>
2025-03-01 18:46:39 +0400

    add RefreshScope

--- src/main/java/com/quick/recording/qrb2bservice/QrB2bServiceApplication.java
+++ src/main/java/com/quick/recording/qrb2bservice/QrB2bServiceApplication.java
@@ -3,11 +3,13 @@ package com.quick.recording.qrb2bservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.quick.recording"})
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.quick.recording.gateway.service")
+@RefreshScope
 public class QrB2bServiceApplication {
 
 	public static void main(String[] args) {
