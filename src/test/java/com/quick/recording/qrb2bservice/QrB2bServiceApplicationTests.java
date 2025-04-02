6c427f813ecd756bf571ae46d4b7348e594389b4
Fort163 <Front-91@mail.ru>
2025-04-02 20:18:20 +0400

    fix test

--- src/test/java/com/quick/recording/qrb2bservice/QrB2bServiceApplicationTests.java
+++ src/test/java/com/quick/recording/qrb2bservice/QrB2bServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.quick.recording.qrb2bservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QrB2bServiceApplicationTests {
 
     @Test
