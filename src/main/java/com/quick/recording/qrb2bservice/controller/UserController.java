96c98f07e05b56f3ffbd24bf8ecca0f6d4e054ce
Fort163 <Front-91@mail.ru>
2025-11-08 17:03:03 +0400

    fix temp controller

--- src/main/java/com/quick/recording/qrb2bservice/controller/UserController.java
+++ src/main/java/com/quick/recording/qrb2bservice/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.quick.recording.qrb2bservice.controller;
 
-import com.quick.recording.gateway.dto.auth.AuthUserDto;
-import com.quick.recording.qrb2bservice.service.NotificationService;
-import com.quick.recording.qrb2bservice.service.UserService;
+import com.quick.recording.qrb2bservice.service.temp.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,21 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
     private final NotificationService notificationService;
-
-    @GetMapping("/createCodeEmail")
-    @PreAuthorize("hasAnyAuthority('ROLE_CHANGE_ME_INFO')")
-    public ResponseEntity<Boolean> createCodeEmail(@RequestParam(name = "email") String email) {
-        return ResponseEntity.ok(notificationService.sendCodeEmail(email));
-    }
-
-    @GetMapping("/checkCodeEmail")
-    @PreAuthorize("hasAnyAuthority('ROLE_CHANGE_ME_INFO')")
-    public ResponseEntity<Boolean> createCodeEmail(@RequestParam(name = "email") String email,
-                                                   @RequestParam(name = "code") String code) {
-        return ResponseEntity.ok(notificationService.checkCodeEmail(email, code));
-    }
 
     @GetMapping("/createCodePhone")
     @PreAuthorize("hasAnyAuthority('ROLE_CHANGE_ME_INFO')")
@@ -40,12 +24,6 @@ public class UserController {
     public ResponseEntity<Boolean> createCodePhone(@RequestParam(name = "phone") String phone,
                                                    @RequestParam(name = "code") String code) {
         return ResponseEntity.ok(notificationService.checkCodePhone(phone, code));
-    }
-
-    @PatchMapping("/change")
-    @PreAuthorize("hasAnyAuthority('ROLE_CHANGE_ME_INFO')")
-    public ResponseEntity<AuthUserDto> change(@RequestBody AuthUserDto userDto) {
-        return userService.changeUser(userDto);
     }
 
 }
