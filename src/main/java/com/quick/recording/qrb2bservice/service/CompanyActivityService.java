0baa8fc378f1ecacb67f97c5155396c37ec4d694
Fort163 <Front-91@mail.ru>
2025-11-08 17:04:17 +0400

    delete deprecated file

--- src/main/java/com/quick/recording/qrb2bservice/service/CompanyActivityService.java
+++
@@ -1,9 +0,0 @@
-package com.quick.recording.qrb2bservice.service;
-
-import com.quick.recording.gateway.dto.company.ActivityDto;
-import com.quick.recording.gateway.main.service.remote.MainRemoteService;
-
-
-public interface CompanyActivityService extends MainRemoteService<ActivityDto> {
-
-}
